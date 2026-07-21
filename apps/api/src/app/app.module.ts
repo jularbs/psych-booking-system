84e06cce32bb811c49567d7f0ce3f3aeae1c0f23
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2026-07-22 01:10:27 +0800

    test(api): add password service with bcrypt hash and verify tdd coverage

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { AuthModule } from '../modules/auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,7 @@ import { HealthModule } from '../modules/health/health.module';
 import { RequestLoggerMiddleware } from '../common/logger/request-logger.middleware';
 import { LoggerModule } from '../common/logger/logger.module';
 @Module({
-  imports: [ConfigurationModule, LoggerModule, HealthModule, DatabaseModule],
+  imports: [ConfigurationModule, LoggerModule, HealthModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
