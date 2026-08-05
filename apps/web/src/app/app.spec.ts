4e796afa13544542d5d0fbd2117df93115d392cb
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2026-08-05 19:07:26 +0800

    test(web): add creation of app testing

--- apps/web/src/app/app.spec.ts
+++ apps/web/src/app/app.spec.ts
@@ -8,4 +8,10 @@ describe('App', () => {
       imports: [App],
     }).compileComponents();
   });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 });
