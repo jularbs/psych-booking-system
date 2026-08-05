0696cbde8549ed9ca1926acc82c1e9e13a60bb72
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2026-08-05 19:05:38 +0800

    test(web): remove test cases from welcome screen

--- apps/web/src/app/app.spec.ts
+++ apps/web/src/app/app.spec.ts
@@ -1,19 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe } from 'vitest';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
     }).compileComponents();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome web');
   });
 });
