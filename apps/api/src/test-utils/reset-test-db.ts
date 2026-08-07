57c80d642886a7ec162c027ae7ac34e9cc4a1e60
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2026-08-07 14:59:11 +0800

    test(api-calendar): add google calendar connection table on reset db helper

--- apps/api/src/test-utils/reset-test-db.ts
+++ apps/api/src/test-utils/reset-test-db.ts
@@ -2,7 +2,7 @@ import { Kysely, sql } from 'kysely';
 
 import { Database } from '../database/database.types';
 
-const TABLES = ['services', 'users'];
+const TABLES = ['services', 'users', 'google_calendar_connections'] as const;
 
 export async function resetTestDb(db: Kysely<Database>) {
   for (const table of TABLES) {
